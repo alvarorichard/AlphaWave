3a7f93a96f33f4462caf97f78e344b6baf042cee
alvarorichard <alvaro10048@protonmail.com>
2023-09-14 18:41:00 -0300

    add sentiment improve

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ from statsmodels.tsa.arima.model import ARIMA
 from datetime import datetime
 
 
+# Função para ajustar a previsão com base no sentimento
+def adjust_forecast_with_sentiment(forecast, sentiment):
+    adjustment_factor = 1 + (sentiment * 0.1)
+    adjusted_forecast = forecast * adjustment_factor
+    return adjusted_forecast
+
+
 # Configurar yfinance para baixar dados
 yf.pdr_override()
 nltk.download('vader_lexicon')
+
 
 # Função para obter dados da News API e analisar o sentimento usando VADER
 def get_sentiment(ticker):
@@ -41,21 +49,24 @@ def get_forecast(ticker):
     most_recent_value = data['Close'].iloc[-1]
     current_year = datetime.now().year
     print(f"Preço atual da ação de {ticker} hoje: ${most_recent_value:.2f}")
+
     annual_mean.loc[current_year] = most_recent_value
     y = annual_mean.values
     model = ARIMA(y, order=(1, 1, 1))
     model_fit = model.fit()
-    years_to_predict = 8  # De 2023 a 2030
+    years_to_predict = 8
     forecast = model_fit.forecast(steps=years_to_predict)
-    sentiment = get_sentiment(ticker)  # Obter dados de sentimento
+    sentiment = get_sentiment(ticker)
+    adjusted_forecast = adjust_forecast_with_sentiment(forecast, sentiment)
 
     # Imprimir previsões no terminal
     for i, year in enumerate(range(current_year, current_year + years_to_predict)):
-        print(f"Previsão para {ticker} em {year}: ${forecast[i]:.2f}")
+        print(f"Previsão sem ajuste para {ticker} em {year}: ${forecast[i]:.2f}")
+        print(f"Previsão ajustada para {ticker} em {year}: ${adjusted_forecast[i]:.2f}")
 
     print(f"Sentimento médio para {ticker}: {sentiment}")
 
-    return annual_mean, forecast, sentiment
+    return annual_mean, forecast, adjusted_forecast, sentiment
 
 
 # Baixar dados e fazer previsões para Apple e Nestlé
@@ -63,8 +74,9 @@ tickers = ['AAPL', 'NSRGY']
 predictions = {}
 
 for ticker in tickers:
-    annual_mean, forecast, sentiment = get_forecast(ticker)
-    predictions[ticker] = {'annual_mean': annual_mean, 'forecast': forecast, 'sentiment': sentiment}
+    annual_mean, forecast, adjusted_forecast, sentiment = get_forecast(ticker)
+    predictions[ticker] = {'annual_mean': annual_mean, 'forecast': forecast, 'adjusted_forecast': adjusted_forecast,
+                           'sentiment': sentiment}
 
 # Plotar os resultados
 plt.figure(figsize=(14, 7))
@@ -75,6 +87,8 @@ for ticker in tickers:
     current_year = datetime.now().year
     plt.plot(range(current_year, current_year + 8), predictions[ticker]['forecast'],
              label=f'Previsão ARIMA de {ticker}', linestyle='dashed', marker='x')
+    plt.plot(range(current_year, current_year + 8), predictions[ticker]['adjusted_forecast'],
+             label=f'Previsão Ajustada de {ticker}', linestyle='dotted', marker='+')
     plt.axhline(y=predictions[ticker]['sentiment'], color='r', linestyle='-',
                 label=f"Sentimento Médio de {ticker}")
 
